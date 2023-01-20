d003aa978658ccec773346624a76525d8ae46f74
Fhaddad9 <ferashaddadgit@gmail.com>
2023-01-20 16:00:33 +0000

    Spring Initalisr added

---
+++ FilmReviewSpring/src/main/java/dev/fhaddad9/FilmReview/FilmReviewApplication.java
@@ -0,0 +1,13 @@
+package dev.fhaddad9.FilmReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmReviewApplication.class, args);
+	}
+
+}
